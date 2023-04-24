29e56202b9adfda6342c08118ead12f8155b851a
brettkanker <stanisic.luka44@gmail.com>
2023-04-24 10:25:07 +0200

    Fixed player prefab and finished clone script

--- Assets/Scripts/CloneScript.cs
+++ Assets/Scripts/CloneScript.cs
@@ -5,16 +5,51 @@ using UnityEngine;
 public class CloneScript : MonoBehaviour
 {
     public GameObject playerPrefab;
+    public Camera playerCamera;
+    public Camera cloneCamera;
+    public Transform bodyTransform;
+
+    private GameObject clone;
+    private bool cloneCreated = false;
+
 
     void Start()
     {
-        Vector3 spawnPosition = transform.position + Vector3.right * 5f;
-        GameObject clone = Instantiate(playerPrefab, spawnPosition, transform.rotation);
-        clone.GetComponentInChildren<Camera>().enabled = false;
+        
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (!cloneCreated)
+            {
+                Vector3 spawnPosition = bodyTransform.position + Vector3.right * 5f;
+                clone = Instantiate(playerPrefab, spawnPosition, transform.rotation);
+
+                // Find the clone camera component in the clone object's hierarchy
+                cloneCamera = clone.GetComponentInChildren<Camera>();
+                cloneCamera.enabled = false;
+
+                // Disable the audio listener of the clone camera
+                AudioListener cloneAudioListener = cloneCamera.GetComponent<AudioListener>();
+                cloneAudioListener.enabled = false;
+
+                cloneCreated = true;
+            }
+            else
+            {
+                // destroy the clone and set cloneCreated to false
+                Destroy(clone);
+                cloneCreated = false;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            // Toggle the player camera and clone camera visibility
+            playerCamera.enabled = !playerCamera.enabled;
+            cloneCamera.enabled = !cloneCamera.enabled;
+        }
     }
 }
